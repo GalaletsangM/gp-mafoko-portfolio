e8c956e754104e8667c85ac79d0c2c85a17d46ed
GalaletsangM <preciousmafoko@gmail.com>
2024-12-13 18:10:59 -0500

    created a responsive navbar

---
+++ script/script.js
@@ -0,0 +1,21 @@
+
+
+function sendEmail() {
+  Email.send({
+    Host: "smtp.gmail.com",
+    Username: "preciousmafoko@gmail.com",
+    Password: "@A.b.c.123",
+    To: "preciousmafoko@gmail.com",
+    From: document.getElementById("email").value,
+    Subject: "New Contact form enquiry",
+    Body:
+      "Name: " +
+      document.getElementById("name").value +
+      "<br> Email: " +
+      document.getElementById("email".value) +
+      "<br> Phone number: " +
+      document.getElementById("phone").value +
+      "<br> Message: " +
+      document.getElementById("message").value,
+  }).then((message) => alert("Message sent succesfully!"));
+}
