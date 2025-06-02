49e911e625699a1f08f2ccfec1ca2c5d80510388
GalaletsangM <preciousmafoko@gmail.com>
2025-06-01 20:16:51 -0400

    new portfolio

--- script/script.js
+++ script/script.js
@@ -2,18 +2,10 @@ function sendEmail() {
   Email.send({
     Host: "smtp.gmail.com",
     Username: "preciousmafoko@gmail.com",
-    Password: "@A.b.c.123",
+    Password: "G@li.1234",
     To: "preciousmafoko@gmail.com",
     From: document.getElementById("email").value,
-    Subject: "New Contact form enquiry",
-    Body:
-      "Name: " +
-      document.getElementById("name").value +
-      "<br> Email: " +
-      document.getElementById("email".value) +
-      "<br> Phone number: " +
-      document.getElementById("phone").value +
-      "<br> Message: " +
-      document.getElementById("message").value,
-  }).then((message) => alert("Message sent succesfully!"));
+    Subject: "New Contact Form Inquiry",
+    Body: "And this is the body",
+  }).then((message) => alert(message));
 }
